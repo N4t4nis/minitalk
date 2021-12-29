74dde6d76160e86ac5e50fb5e9eb33b5d602a049
Nathan BENHADOUCHE <nbenhado@e3r4p23.clusters.42paris.fr>
2021-12-29 20:41:02 +0100

    mise en ecoute du serveur et initialisations

--- client.c
+++ client.c
@@ -6,9 +6,46 @@
 /*   By: nbenhado <nbenhado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 15:30:05 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/29 15:30:06 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/29 20:14:08 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    client(int pid, char *str)
-{}
+#include "ft_printf/ft_printf.h"
+#include "libft/libft.h"
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+
+void            client(int pid, char *str)
+{
+    // envoie le signal SIGUSR1 au pid (donc au serveur), return 0 si ok, sinon -1
+    if (kill(pid, SIGUSR1))
+        ft_putstr_fd(str, 1);
+    else
+        printf("rate");
+}
+
+//indique ce que fait SIGUSR1 ou SIGUSR2 (gestionnaire)
+// parametre = signal qui l'a activee (1 ou 2 dans notre cas)
+static void handler(int signo)
+{
+    if (signo == SIGUSR1)
+        printf("message recue bg\n");
+
+}
+
+
+int main(int ac, char **av)
+{
+    //client(ft_atoi(av[1]), av[2]);   
+    kill(ft_atoi(av[1]), SIGUSR1);
+}
+
+
+
+
--- server.c
+++ server.c
@@ -6,23 +6,53 @@
 /*   By: nbenhado <nbenhado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 15:23:57 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/29 15:31:46 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/29 20:36:36 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 
 #include <sys/types.h>
+#include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
 
+/*
+L'appel système kill() peut être utilisé pour envoyer n'importe quel 
+signal à n'importe quel processus ou groupe de processus. 
+
+SIGUSR1 User defined signal 1 : Définie par le programmeur. L'action par défaut est la
+mise à mort du processus.
+*/
 void    serveur()
 {
     int pid_c = getpid();
-    ft_printf("PID SERVER : %d\n", pid_c);
+    ft_printf("%d\n", pid_c);
 }
 
-int main()
+//indique ce que fait SIGUSR1 ou SIGUSR2 (gestionnaire)
+// parametre = signal qui l'a activee (1 ou 2 dans notre cas)
+static void handler(int signo)
 {
-    serveur();    
+    if (signo == SIGUSR1)
+        printf("test\n");
+
+}
+
+
+int main() 
+{   
+    /*Un signal est une notification asynchrone envoyée à un processus pour lui     signaler l'apparition d'un événement.
+    Signal() renvoie un pointeur vers handler()*/
+    int car;
+    struct sigaction act;
+    
+    act.sa_handler = handler;
+    sigaction(SIGUSR1, &act, NULL);
+    read(0, &car, sizeof(car)) <0) 
+    
+    int pid_c = getpid();
+    ft_printf("listening... %d\n", pid_c);
+    ft_printf("terminating... %d\n", pid_c);
+    while (1) pause();
 }
